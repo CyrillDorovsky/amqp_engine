57428f1efa27fcc352a32eaa73c74ea96d5e15ac
Cyrill Dorovsky <cyrill.dorovsky@gmail.com>
2015-06-18 14:10:18 +0300

    debil

--- lib/amqp_engine/direct_offer.rb
+++ lib/amqp_engine/direct_offer.rb
@@ -6,8 +6,8 @@ class DirectOffer
 
   def work( msg )
     event = Event.new( msg )
-    mongohq_url = ENV['MONGOHQ_URL'] || [ '127.0.0.1:27017' ]
-    mongo_client = Moped::Session.new( mongohq_url )
+    mongohq_url = ENV['MONGOHQ_URL'] || '127.0.0.1:27017'
+    mongo_client = Moped::Session.new( [ mongohq_url ] )
     mongo_client.use( 'api_events' )
     mongo_client.with( safe: true ) do | session |
       session[ event.mongo_db ].insert event.adapt
--- lib/amqp_engine/pg_import.rb
+++ lib/amqp_engine/pg_import.rb
@@ -13,8 +13,8 @@ class PgImport
     configuration = YAML::load(IO.read('config/database.yml'))
     ActiveRecord::Base.establish_connection( configuration[ env ] )
 
-    mongo_url = mongohq_url = ENV['MONGOHQ_URL'] || [ '127.0.0.1:27017' ]
-    mongo_client = Moped::Session.new( mongohq_url )
+    mongo_url = mongohq_url = ENV['MONGOHQ_URL'] || '127.0.0.1:27017'
+    mongo_client = Moped::Session.new( [ mongohq_url ] )
     mongo_client.use( 'api_events' )
 
     collections = collections_for( msg )

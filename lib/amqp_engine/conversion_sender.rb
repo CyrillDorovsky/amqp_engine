110e48bcba9342d4cf9f8545d8142059f2ce5b85
user <rentcasey@gmail.com>
2015-07-05 19:45:35 +0300

    handling errors

--- lib/amqp_engine/conversion_sender.rb
+++ lib/amqp_engine/conversion_sender.rb
@@ -22,20 +22,20 @@ class ConversionSender
       ActiveRecord::Base.establish_connection( configuration[ env ] )
     end
     begin
-    id = case msg
-      when 'mongo_offer_set_show'
-        mongo_client[ 'mongo_event_atomics' ].where( event_name: 'offer_set', dealer_id: 1 ).first['_id'].to_s
-      when 'pg_offer_set_show'
-        OfferSetShow.not_converted.where( dealer_id: 1 ).first.mongo_id.to_s
-      when 'pg_direct_offer'
-        DirectOfferRedirect.not_converted.where( dealer_id: 1 ).first.request_id
-      end
+      id = case msg
+        when 'mongo_offer_set_show'
+          mongo_client[ 'mongo_event_atomics' ].where( event_name: 'offer_set', dealer_id: 1 ).last['_id'].to_s
+        when 'pg_offer_set_show'
+          OfferSetShow.not_converted.where( dealer_id: 1 ).first.mongo_id.to_s
+        when 'pg_direct_offer'
+          DirectOfferRedirect.not_converted.where( dealer_id: 1 ).first.request_id
+        end
+      conversion_url = "http://api.richpays.com/apps_advert/mobvistadirect?placement=#{ id }"
+      RestClient.get conversion_url if id
+      ack!
     rescue => e
       puts e
     end
-    conversion_url = "http://api.richpays.com/apps_advert/glispa?placement=#{ id }"
-    RestClient.get conversion_url
-    ack!
   end
 
   add_transaction_tracer :work, :category => :task
